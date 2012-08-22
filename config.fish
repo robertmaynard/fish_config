374ca34dc9e9fc1eb5045fbeb981ec7def6805c6
Robert Maynard <robert.maynard@kitware.com>
2012-08-22 17:10:41 -0400

    Update config with git branch in the PS1.

--- config.fish
+++ config.fish
@@ -3,3 +3,23 @@ if status --is-login
 	set -xU CC /usr/local/bin/gcc-4.7
 	set -xU CXX /usr/local/bin/g++-4.7
 end
+
+function parse_git_branch
+	sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+end
+
+
+function fish_prompt -d "Write out the prompt"
+	# Color writeable dirs green, read-only dirs red
+	if test -w "."
+		printf ' %s%s' (set_color green) (prompt_pwd)
+	else
+		printf ' %s%s' (set_color red) (prompt_pwd)
+	end
+
+	# Print git branch
+	if test -d ".git"
+		printf ' %s%s/%s' (set_color normal) (set_color blue) (parse_git_branch)
+	end
+	printf '%s> ' (set_color normal)
+end
