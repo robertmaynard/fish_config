b33b9e5bea245a6116e5398f98fc087d1961491f
Robert Maynard <robert.maynard@kitware.com>
2013-12-11 11:32:00 -0500

    setup clang as the default compiler on unix

--- config.fish
+++ config.fish
@@ -1,11 +1,8 @@
 if status --is-login
 	set PATH /usr/local/bin /bin /usr/sbin /sbin /usr/bin /usr/local/cuda/bin
 	set -xU EDITOR 'subl -w'
-	
-	#setup rbenv
-	set -x PATH $HOME/.rbenv/shims $PATH
-	set -x PATH $HOME/.rbenv/bin $PATH
-	rbenv rehash >/dev/null ^&1
+	set -xU CC 'clang'
+	set -xU CXX 'clang++'	
 end
 
 function parse_git_branch
@@ -28,17 +25,3 @@ function fish_prompt -d "Write out the prompt"
 	printf '%s> ' (set_color normal)
 end
 
-function rbenv_shell
-  set -l vers $argv[1]
-
-  switch "$vers"
-    case '--complete'
-      echo '--unset'
-      echo 'system'
-      exec rbenv-versions --bare
-      return
-    case '--unset'
-      set -e RBENV_VERSION
-      return 1
-  end
-end
