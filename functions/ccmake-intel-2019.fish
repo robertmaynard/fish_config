288f0627601434422aaba61fc33508665bbe5551
Robert Maynard <robert.maynard@kitware.com>
2019-11-15 15:18:03 -0500

    Add cmake bindings for Intel 2019

---
+++ functions/ccmake-intel-2019.fish
@@ -0,0 +1,5 @@
+function ccmake-intel-2019
+  set -lx CC /opt/intel/compilers_and_libraries_2019/linux/bin/intel64/icc
+  set -lx CXX /opt/intel/compilers_and_libraries_2019/linux/bin/intel64/icpc
+  ccmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $argv
+end
