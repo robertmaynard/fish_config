d37b7cebe1a1ccc8024e8939d53950b63f965ed0
Robert Maynard <robert.maynard@kitware.com>
2019-12-31 16:17:33 -0500

    setup global ccache hooks for CMake 3.17

--- config.fish
+++ config.fish
@@ -3,6 +3,10 @@ set -x fish_user_paths /usr/local/cuda/bin /home/robert/.local/bin/
 set -x LD_LIBRARY_PATH "/usr/local/cuda/lib64"
 set -x CMAKE_GENERATOR Ninja
 
+#useful only for CMake 3.17+
+set -x CMAKE_C_COMPILER_LAUNCHER ccache
+set -x CMAKE_CXX_COMPILER_LAUNCHER ccache
+
 #setup keychain
 if status --is-interactive
     keychain --quiet --agents ssh --eval id_rsa
