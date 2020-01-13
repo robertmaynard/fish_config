60231673c6807ea50063519d56cfb8f3198c00aa
Robert Maynard <robert.maynard@kitware.com>
2020-01-13 14:12:16 -0500

    correct typos in cmake-clang6

--- functions/cmake-clang6.fish
+++ functions/cmake-clang6.fish
@@ -1,4 +1,4 @@
-function cmake-clang6-DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+function cmake-clang6
 	set -lx CC /usr/bin/clang-6.0
 	set -lx CXX /usr/bin/clang++-6.0
 	cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $argv
