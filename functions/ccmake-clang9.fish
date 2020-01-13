ac4ca15e05250ce2c3dbf1f03f7a14fd15dccd33
Robert Maynard <robert.maynard@kitware.com>
2020-01-13 14:47:53 -0500

    Add bindings for clang-9

---
+++ functions/ccmake-clang9.fish
@@ -0,0 +1,5 @@
+function ccmake-clang9
+  set -lx CC /usr/bin/clang-9
+  set -lx CXX /usr/bin/clang++-9
+  ccmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $argv
+end
---
+++ functions/cmake-clang9.fish
@@ -0,0 +1,5 @@
+function cmake-clang9
+	set -lx CC /usr/bin/clang-9
+	set -lx CXX /usr/bin/clang++-9
+	cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $argv
+end
