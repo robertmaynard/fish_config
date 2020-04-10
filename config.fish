74fb5339ae1c512415877a18b37307a76fab74e0
Robert Maynard <robert.maynard@kitware.com>
2020-04-10 08:47:03 -0400

    Increase the sccache cache size

--- config.fish
+++ config.fish
@@ -3,6 +3,8 @@ set -x fish_user_paths /usr/local/cuda/bin /home/robert/.local/bin/
 set -x LD_LIBRARY_PATH "/usr/local/cuda/lib64"
 set -x CMAKE_GENERATOR Ninja
 
+set -x SCCACHE_CACHE_SIZE "32G"
+
 #useful only for CMake 3.17+
 set -x CMAKE_C_COMPILER_LAUNCHER sccache
 set -x CMAKE_CXX_COMPILER_LAUNCHER sccache
