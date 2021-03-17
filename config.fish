9c660e9af0d04fcd05795d6bb0fa6a3f52efd6fd
Robert Maynard <rmaynard@nvidia.com>
2021-03-17 08:22:34 -0700

    Update ubuntu_1804 to prefered 2021 settings

--- config.fish
+++ config.fish
@@ -1,18 +1,17 @@
 
-set -x fish_user_paths /usr/local/cuda/bin /home/robert/.local/bin/
 set -x LD_LIBRARY_PATH "/usr/local/cuda/lib64"
 set -x CMAKE_GENERATOR Ninja
 
 set -x SCCACHE_CACHE_SIZE "32G"
 
 #useful only for CMake 3.17+
-set -x CMAKE_C_COMPILER_LAUNCHER sccache
-set -x CMAKE_CXX_COMPILER_LAUNCHER sccache
-set -x CMAKE_CUDA_COMPILER_LAUNCHER sccache
+set -x CMAKE_C_COMPILER_LAUNCHER ccache
+set -x CMAKE_CXX_COMPILER_LAUNCHER ccache
+set -x CMAKE_CUDA_COMPILER_LAUNCHER ccache
 
 #setup keychain
 if status --is-interactive
-    keychain --quiet --agents ssh --eval id_rsa
+    keychain --quiet --agents ssh --eval id_ed25519
 end
 
 begin
@@ -23,9 +22,9 @@ begin
 end
 
 
-# added by pipx (https://github.com/pipxproject/pipx)
-set -x PATH /home/robert/.local/bin $PATH
-
 # for rust
-set -x PATH /home/robert/.cargo/bin $PATH
+if test ~/.cargo/bin
+	set -x PATH $PATH ~/.cargo/bin
+end
+
 set -x CCMAKE_COLORS "s=213:p=227:c=207:n=203:y=49"
